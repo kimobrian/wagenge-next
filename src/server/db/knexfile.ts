8cf21fcb67289bdcd323cbac64df61e43b76645f
Kimo <kimocoding@gmail.com>
2024-07-21 00:28:16 +0300

    resetup node modules

--- src/server/db/knexfile.ts
+++ src/server/db/knexfile.ts
@@ -1,15 +1,7 @@
 import type { Knex } from "knex";
-// import * as dotenv from "dotenv";
-import * as path from "path";
 import { config as env } from "../env";
 
-// const envFile = process.env.NODE_ENV === "production" ? ".env" : ".env.local";
-// console.log("Path::", path.resolve(__dirname, `../../../${envFile}`));
-// dotenv.config({ path: path.resolve(__dirname, `../../../${envFile}`) });
-
 const { dbUser, dbPass, dbName, dbHost } = env;
-console.log("🚀 ~ DB_USERNAME:", dbUser, dbName, dbPass, dbHost);
-// Update with your config settings.
 
 const config: { [key: string]: Knex.Config } = {
   development: {
